ddeede03d9b3c5378bd72923fe39f547926ec818
wunluv <santhan@mojahmedia.net>
2024-10-26 14:25:44 +0200

    updating dev and prod versions of config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
-import { postcss } from 'tailwindcss';
 
 // https://astro.build/config
 export default defineConfig({
